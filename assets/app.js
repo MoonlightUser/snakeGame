71aec32fdaff5bcdaa2a650e6cef18b2d08f471b
Tymur <tymurmarenych@gmail.com>
2022-06-18 22:50:16 +0100

    fix some problem and for first make it in css

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,16 @@
-for (let hight = 0; hight == 10; hight++){ //render map in hight
-    const newArea = document.createElement("div")
-    newArea.className = "map"
-    newArea.style.top = `${hight*10}`
+for (let weidth = 0; weidth <= 10; weidth++){ //render map in hight
+    let paddingTop = weidth * 30
+    let newAreaW = document.createElement("div")
+    newAreaW.className = "map"
+    newAreaW.style.top = `${paddingTop}px`
+    document.body.appendChild(newAreaW)
 
-    for (let weidth = 0; weidth == 10; weidth++){ //render map in weidth
-        const newArea = document.createElement("div")
-        newArea.className = "map"
-        newArea.style.left = `${weidth*10}`
+    for (let hight = 0; hight <= 10; hight++){ //render map in weidth
+        let paddingLeft = hight * 30
+        let newAreaH = document.createElement("div")
+        newAreaH.className = "map"
+        newAreaH.style.top = `${paddingTop}px`
+        newAreaH.style.left = `${paddingLeft}px` 
+        document.body.appendChild(newAreaH)
     }
 }
