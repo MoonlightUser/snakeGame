cef73b9417861c5957a929d5fef884c384ca62f2
Tymur <tymurmarenych@gmail.com>
2022-06-18 22:11:39 +0100

    create cycle for render height bloks

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+for (let hight = 0; hight == 10; hight++){ //render map in hight and weidth
+    const newArea = document.createElement("div")
+    newArea.className = "map"
+    newArea.style.top = `${hight*10}`
+}
