dcde573b880f97707a92e11caeff21e3beb4925e
Tymur <tymurmarenych@gmail.com>
2022-06-18 22:13:03 +0100

    create cycle for render weidth bloks

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,11 @@
-for (let hight = 0; hight == 10; hight++){ //render map in hight and weidth
+for (let hight = 0; hight == 10; hight++){ //render map in hight
     const newArea = document.createElement("div")
     newArea.className = "map"
     newArea.style.top = `${hight*10}`
+
+    for (let weidth = 0; weidth == 10; weidth++){ //render map in weidth
+        const newArea = document.createElement("div")
+        newArea.className = "map"
+        newArea.style.left = `${weidth*10}`
+    }
 }
