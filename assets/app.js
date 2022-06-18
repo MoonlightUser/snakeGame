2e65acfb2095dd6b56089afcc9228efcc05f4045
Tymur <tymurmarenych@gmail.com>
2022-06-19 00:48:03 +0100

    create apple. apple can teleport in random place

--- assets/app.js
+++ assets/app.js
@@ -2,30 +2,37 @@ const m = 30 // 1m = 30px
 
 let keyNow = "w"
 let snake_head_position = {
-    width: 2*m,
-    height: 2*m
+    y: 2*m,
+    x: 2*m
 }
 
 let apple_position = {
-    width: 7*m,
-    height: 4*m
+    x: 7*m,
+    y: 4*m
 }
 const snake_head = document.createElement("div")
 snake_head.id = "snake_HEAD"
 snake_head.className = "snake"
-snake_head.style.top = `${snake_head_position.height}px`
-snake_head.style.left = `${snake_head_position.width}px `
+snake_head.style.top = `${snake_head_position.x}px`
+snake_head.style.left = `${snake_head_position.y}px `
 document.body.appendChild(snake_head)
 
-for (let width = 0; width <= 10; width++){ //render map in hight
-    let paddingTop = width * m
+const apple = document.createElement("div")
+apple.id = "apple_id"
+apple.className = "apple"
+apple.style.top = `${apple_position.y}px`
+apple.style.left = `${apple_position.x}px `
+document.body.appendChild(apple)
+
+for (let x = 0; x <= 10; x++){ //render map in hight
+    let paddingTop = x * m
     let newAreaW = document.createElement("div")
     newAreaW.className = "map"
     newAreaW.style.top = `${paddingTop}px`
     document.body.appendChild(newAreaW)
 
-    for (let height = 0; height <= 10; height++){ //render map in weidth
-        let paddingLeft = height * m
+    for (let y = 0; y <= 10; y++){ //render map in weidth
+        let paddingLeft = y * m
         let newAreaH = document.createElement("div")
         newAreaH.className = "map"
         newAreaH.style.top = `${paddingTop}px`
@@ -56,31 +63,37 @@ document.addEventListener("keydown", (key) =>{
         game()
     }
 })
-
+function checkAppple(){
+    // console.log(snake_head_position.y,  apple_position.y, "y  **  x", snake_head_position.x, apple_position.x);
+    if (snake_head_position.y == apple_position.y && snake_head_position.x == apple_position.x){
+        console.log("numnum");
+    }
+}
 function game(){
     if (keyNow == "w"){
-        snake_head_position.width -= m
+        snake_head_position.y -= m
         renderSnake()
     }
     else if (keyNow == "a"){
-        snake_head_position.height -= m
+        snake_head_position.x -= m
         renderSnake()
     }
     else if (keyNow == "d"){
-        snake_head_position.height += m
+        snake_head_position.x += m
         renderSnake()
     }
     else if (keyNow == "s"){
-        snake_head_position.width += m
+        snake_head_position.y += m
         renderSnake()
     }
+    checkAppple()
 }
 
 function renderSnake(){
     let snake_head = document.getElementById("snake_HEAD")
     snake_head.className = "snake"
-    snake_head.style.top = `${snake_head_position.width}px`
-    snake_head.style.left = `${snake_head_position.height}px`
+    snake_head.style.top = `${snake_head_position.y}px`
+    snake_head.style.left = `${snake_head_position.x}px`
     document.body.appendChild(snake_head)
 }
 function sleep(ms) {
