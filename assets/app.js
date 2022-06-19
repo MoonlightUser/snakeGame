10e6e65a81c968b17e1f766bef89e3c5b7dbcc88
Tymur <tymurmarenych@gmail.com>
2022-06-19 01:08:50 +0100

    Squashed commit of the following:
    commit 0d79b1e8ed2612aeb6abee3fd40faabf983f27bf
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sun Jun 19 00:49:25 2022 +0100
    
        create apple. apple can teleport in random place
    
    commit 2e65acfb2095dd6b56089afcc9228efcc05f4045
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sun Jun 19 00:48:03 2022 +0100
    
        create apple. apple can teleport in random place
    
    commit 95ff50f7c61d4337f349baa7fd18833bfa9d52ad
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sun Jun 19 00:17:36 2022 +0100
    
        add controll of snake
    
    commit f47952d1d0f78a0f9034d105d80a5989f8796ad1
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sat Jun 18 23:38:43 2022 +0100
    
        make snake on site
    
    commit d6c93e6d082f72592354775d5798b3753c6414a1
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sat Jun 18 23:23:59 2022 +0100
    
        Squashed commit of the following:
    
        commit 93d9dcb6430f1225dbf55df78687387003d4b180
        Author: Tymur <tymurmarenych@gmail.com>
        Date:   Sat Jun 18 23:20:57 2022 +0100
    
            now we can listen
    
    commit 71aec32fdaff5bcdaa2a650e6cef18b2d08f471b
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sat Jun 18 22:50:16 2022 +0100
    
        fix some problem and for first make it in css
    
    commit dcde573b880f97707a92e11caeff21e3beb4925e
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sat Jun 18 22:13:03 2022 +0100
    
        create cycle for render weidth bloks
    
    commit cef73b9417861c5957a929d5fef884c384ca62f2
    Author: Tymur <tymurmarenych@gmail.com>
    Date:   Sat Jun 18 22:11:39 2022 +0100
    
        create cycle for render height bloks

---
+++ assets/app.js
@@ -0,0 +1,118 @@
+const m = 30 // 1m = 30px 
+
+let keyNow = "w"
+let snake_head_position = {
+    y: 2*m,
+    x: 2*m
+}
+
+let apple_position = {
+    x: 7*m,
+    y: 4*m
+}
+const snake_head = document.createElement("div")
+snake_head.id = "snake_HEAD"
+snake_head.className = "snake"
+snake_head.style.top = `${snake_head_position.x}px`
+snake_head.style.left = `${snake_head_position.y}px `
+document.body.appendChild(snake_head)
+
+const apple = document.createElement("div")
+apple.id = "apple_id"
+apple.className = "apple"
+apple.style.top = `${apple_position.y}px`
+apple.style.left = `${apple_position.x}px `
+document.body.appendChild(apple)
+
+for (let x = 0; x <= 10; x++){ //render map in hight
+    let paddingTop = x * m
+    let newAreaW = document.createElement("div")
+    newAreaW.className = "map"
+    newAreaW.style.top = `${paddingTop}px`
+    document.body.appendChild(newAreaW)
+
+    for (let y = 0; y <= 10; y++){ //render map in weidth
+        let paddingLeft = y * m
+        let newAreaH = document.createElement("div")
+        newAreaH.className = "map"
+        newAreaH.style.top = `${paddingTop}px`
+        newAreaH.style.left = `${paddingLeft}px` 
+        document.body.appendChild(newAreaH)
+    }
+}
+
+document.addEventListener("keydown", (key) =>{
+    if (`${key.key}` == "w" || `${key.key}` == "W"){
+        console.log('%capp.js line:20 "w"', 'color: #007acc;', "w");
+        keyNow = "w"
+        game()
+    }
+    else if (`${key.key}` == "a" || `${key.key}` == "A"){
+        console.log('%capp.js line:20 "w"', 'color: #007acc;', "a");
+        keyNow = "a"
+        game()
+    }
+    else if (`${key.key}` == "d" || `${key.key}` == "D"){
+        console.log('%capp.js line:20 "w"', 'color: #007acc;', "d");
+        keyNow = "d"
+        game()
+    }
+    else if (`${key.key}` == "s" || `${key.key}` == "S"){
+        console.log('%capp.js line:20 "w"', 'color: #007acc;', "s");
+        keyNow = "s"
+        game()
+    }
+})
+function checkAppple(){
+    if (snake_head_position.y == apple_position.y && snake_head_position.x == apple_position.x){
+        console.log("numnum");
+        changeApple()
+    }
+}
+function game(){
+    if (keyNow == "w"){
+        snake_head_position.y -= m
+        renderSnake()
+    }
+    else if (keyNow == "a"){
+        snake_head_position.x -= m
+        renderSnake()
+    }
+    else if (keyNow == "d"){
+        snake_head_position.x += m
+        renderSnake()
+    }
+    else if (keyNow == "s"){
+        snake_head_position.y += m
+        renderSnake()
+    }
+    checkAppple()
+}
+
+function renderSnake(){
+    let snake_head = document.getElementById("snake_HEAD")
+    snake_head.className = "snake"
+    snake_head.style.top = `${snake_head_position.y}px`
+    snake_head.style.left = `${snake_head_position.x}px`
+    document.body.appendChild(snake_head)
+}
+function sleep(ms) {
+    console.log("object");
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function changeApple(){
+    apple_position = {
+        x: Math.floor(Math.random() * 11)*m,
+        y: Math.floor(Math.random() * 11)*m
+    }
+    apple.style.top = `${apple_position.y}px`
+    apple.style.left = `${apple_position.x}px `
+    document.body.appendChild(apple)
+}
+
+
+for (let i = 0; i <= 10; i++){
+    
+
+}
